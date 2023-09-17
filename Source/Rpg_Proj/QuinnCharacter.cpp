5a5bc84748857fcc0632eadc33c958c17866edad
Aventador <ws419460185@gmail.com>
2023-09-17 23:57:49 +0800

    Add Camera Control and Movement

--- Source/Rpg_Proj/QuinnCharacter.cpp
+++ Source/Rpg_Proj/QuinnCharacter.cpp
@@ -7,18 +7,25 @@
 #include "GameFramework/SpringArmComponent.h"
 #include "EnhancedInputComponent.h"
 #include "EnhancedInputSubsystems.h"
+#include "GameFramework/CharacterMovementComponent.h"
 
 AQuinnCharacter::AQuinnCharacter()
 {
 	PrimaryActorTick.bCanEverTick = true;
+	
+	bUseControllerRotationYaw = false;
 
+	GetCharacterMovement()->bOrientRotationToMovement = true;
+	GetCharacterMovement()->RotationRate = FRotator(0.f,1000.f,0.f);
+	
 	CameraBoom = CreateDefaultSubobject<USpringArmComponent>("SpringArm");
-	CameraBoom->SetupAttachment(GetMesh());
+	CameraBoom->SetupAttachment(GetRootComponent());
 	CameraBoom->TargetArmLength = 500.f;
+	CameraBoom->bUsePawnControlRotation = true;
 
 	FollowCamera = CreateDefaultSubobject<UCameraComponent>("FollowCamera");
 	FollowCamera->SetupAttachment(CameraBoom,USpringArmComponent::SocketName);
-
+	
 }
 
 void AQuinnCharacter::BeginPlay()
@@ -32,7 +39,11 @@ void AQuinnCharacter::BeginPlay()
 			Subsystem->AddMappingContext(DefaultInputMappingContext,0);
 		}
 	}
-	
+
+	if (APlayerController* PlayerController = Cast<APlayerController>(Controller))
+	{
+		PlayerController->RotationInput -= FRotator(30.f, 0.f, 0.f);
+	}
 }
 
 
@@ -57,21 +68,27 @@ void AQuinnCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComp
 
 void AQuinnCharacter::Move(const FInputActionValue& Value)
 {
-	FVector2D MovementVector = Value.Get<FVector2D>();
+	
 	if (Controller)
 	{
+		FVector2D MovementVector = Value.Get<FVector2D>();
 		FRotator Rotation = Controller->GetControlRotation();
 		FRotator YawRotation(0.f, Rotation.Yaw, 0.f);
 		const FVector ForwardDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
 		const FVector RightDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
 		AddMovementInput(ForwardDirection,MovementVector.X);
 		AddMovementInput(RightDirection,MovementVector.Y);
-		
 	}
 } 
 
 void AQuinnCharacter::Look(const FInputActionValue& Value)
 {
+	if (Controller)
+	{
+		FVector2D LookVector = Value.Get<FVector2D>();
+		AddControllerYawInput(LookVector.X);
+		AddControllerPitchInput(LookVector.Y);
+	}
 }
 
 
